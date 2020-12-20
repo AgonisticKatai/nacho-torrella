44ba85cbaa5f363c3a169e14535cc0458b2d5fc8
Nacho Torrella <ignacio.torrella@schibsted.com>
2020-12-20 10:12:18 +0100

    best quality image

--- pages/index.js
+++ pages/index.js
@@ -8,11 +8,13 @@ export default function Home () {
     <div className={styles.container}>
       <Head>
         <title>Nacho Torrella</title>
+        <meta name="viewport" content="initial-scale=1.0, width=device-width" />
+        <meta name="description" content="Frontend Engineer Portfolio" />
         <link rel="icon" href="/favicon.ico" />
       </Head>
 
       <main className={styles.main}>
-        <Image layout="fill" className={styles.profileImage} src="/images/profile-image.jpg" alt="Nacho Torrella"/>
+        <Image layout="fill" className={styles.profileImage} src="/images/profile-image.jpg" alt="Nacho Torrella" quality={100}/>
         <div className={styles.titleWrapper}>
           <h1 className={styles.title}>Nacho Torrella</h1>
           <p className={styles.description}>Frontend Engineer</p>
