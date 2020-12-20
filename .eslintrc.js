09a8242d8984511dc51784e9c37c5fee4856bd8f
Nacho Torrella <ignacio.torrella@schibsted.com>
2020-12-20 01:38:09 +0100

    home page

---
+++ .eslintrc.js
@@ -0,0 +1,32 @@
+const RULES = {
+  OFF: 'off',
+  WARN: 'warn',
+  ERROR: 'error',
+  ALWAYS: 'always',
+  MULTILINE: 'multiline'
+}
+
+module.exports = {
+  env: {
+    browser: true,
+    es2021: true
+  },
+  extends: [
+    'plugin:react/recommended',
+    'standard'
+  ],
+  parserOptions: {
+    ecmaFeatures: {
+      jsx: true
+    },
+    ecmaVersion: 12,
+    sourceType: 'module'
+  },
+  plugins: [
+    'react'
+  ],
+  rules: {
+    'react/react-in-jsx-scope': RULES.OFF,
+    'react/prop-types': RULES.OFF
+  }
+}
